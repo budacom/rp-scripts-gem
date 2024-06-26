395ca34cc1a3b66a776abb88a049c1d1a81fb6a1
Ignacio Baixas <ignacio@buda.com>
2024-06-26 18:53:09 -0400

    feat: adds Session.reusable? method

--- spec/rp_scripts/session_spec.rb
+++ spec/rp_scripts/session_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe RpScripts::Session, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:identifier) }
   end
+
+  describe '#reusable?' do
+    it 'returns false if reusable_until is nil' do
+      session.reusable_until = nil
+      expect(session.reusable?).to be false
+    end
+
+    it 'returns false if reusable_until is less than current time' do
+      session.reusable_until = 1.second.ago
+      expect(session.reusable?).to be false
+    end
+
+    it 'returns true if reusable_until is greater than current time' do
+      session.reusable_until = 1.second.from_now
+      expect(session.reusable?).to be true
+    end
+  end
 end
